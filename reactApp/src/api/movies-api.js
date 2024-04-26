9e2297e63b90cf54de16091988b740269e849e1e
OzzyD1 <ozzydomarkas@gmail.com>
2024-04-26 12:39:38 +0100

    Topic 12 - Integrate Users Endpoints

---
+++ reactApp/src/api/movies-api.js
@@ -0,0 +1,31 @@
+export const getMovies = async () => {
+    const response = await fetch(
+        `https://api.themoviedb.org/3/discover/movie?api_key=8edd9986e017e8a1171ef80c76002e77&language=en-US&include_adult=false&page=1`
+    );
+    return response.json();
+};
+
+export const login = async (username, password) => {
+    const response = await fetch("http://localhost:8080/api/users", {
+        headers: {
+            "Content-Type": "application/json",
+        },
+        method: "post",
+        body: JSON.stringify({ username: username, password: password }),
+    });
+    return response.json();
+};
+
+export const signup = async (username, password) => {
+    const response = await fetch(
+        "http://localhost:8080/api/users?action=register",
+        {
+            headers: {
+                "Content-Type": "application/json",
+            },
+            method: "post",
+            body: JSON.stringify({ username: username, password: password }),
+        }
+    );
+    return response.json();
+};
